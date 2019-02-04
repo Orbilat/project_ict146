7c8b88ad2c9f2f6f042ad1697ed63b5dac4b4ebf
Orbilat <arjuneorbiso@gmail.com>
2019-02-04 20:02:06 +0800

    initial commit for admin branch - view edits

---
+++ resources/views/table_style.blade.php
@@ -0,0 +1,22 @@
+<style>
+    table {
+      font-family: arial, sans-serif;
+      border-collapse: collapse;
+      width: 100%;
+    }
+    
+    td {
+      border: 1px solid #dddddd;
+      text-align: left;
+      padding: 8px;
+    }
+    th {
+      border: 1px solid #dddddd;
+      text-align: left;
+      padding: 8px;
+      background-color: #f2f2f2;
+    }
+    p {
+        font-family: 'Times New Roman', Times, serif;
+    }
+</style>
