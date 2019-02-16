62b38a6fc238ff479c77f0afb33a72afeff74f41
Klevin Cinco <klevincinco@gmail.com>
2019-02-16 19:31:49 +0800

    UPDATE

--- app/Event.php
+++ app/Event.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     protected $fillable = [
-        'event_name', 'start_date', 'end_date',
+        'title','start_date','end_date'
     ];
 }
--- resources/views/layouts/clientapp.blade.php
+++ resources/views/layouts/clientapp.blade.php
@@ -11,12 +11,6 @@
 
   
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> 
-    <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.js"></script>
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.css"/>
     
 
     <!-- Fonts -->
@@ -29,14 +23,19 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <!-- Client Style -->
     @include('layouts/client_homestyle')
-
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.css"/>
 
 </head>
 <body>
         <main>
             @yield('content')
         </main>
-       
+         <!-- Scripts -->
+         <script src="http://code.jquery.com/jquery.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.js"></script>
+ 
+{!! $calendar_details->script() !!}
 </body>
 </html>
