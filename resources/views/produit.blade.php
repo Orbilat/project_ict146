ccaa3a4f7e402c996fbc6aa8aa01ebda44dfedbf
daryne28 <darynemayol28@gmail.com>
2019-02-22 20:27:59 -0600

    Update 4

---
+++ resources/views/produit.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Barcode</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <style>
+        .row{
+            margin:0px;
+        }
+        h2{
+            margin-top:60px;
+        }
+    </style>
+</head>
+<body>
+    <div class="row">
+        @foreach($produits as $p)
+        <div class="col-md-4">
+            <div>{!! DNS1D::getBarcodeHTML ($p->barcode, 'C128A') !!}</div>
+            <h2>{{ $p->name}}</h2>
+        </div>
+        @endforeach
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('barcode','ProduitController@index');
+
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
