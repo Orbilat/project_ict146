a40e6204e400e6e10c1774cb18ecc7ad1c9d6317
LiteInterns <liteinterns@gmail.com>
2019-04-02 15:48:15 +0800

    finished seeder in employee

--- database/seeds/EmployeesTableSeeder.php
+++ database/seeds/EmployeesTableSeeder.php
@@ -14,15 +14,39 @@ class EmployeesTableSeeder extends Seeder
     public function run()
     {
         DB::table('employees')->insert([
-            'username'  => 'tester',
-            'password'  => bcrypt('tester'),
-            'employeeName' => 'Test Admin',
-            'position'  => 'Admin',
-            'idNumber'  => '14102706',
-            'licenseNumber' => '14102706',
-            'userType'  => 'administrator',
-            'created_at' => new DateTime,
-            'updated_at' => new DateTime,
+            [
+                'username'  => 'tester',
+                'password'  => bcrypt('tester'),
+                'employeeName' => 'Test Admin',
+                'position'  => 'Admin',
+                'idNumber'  => '14102706',
+                'licenseNumber' => '14102706',
+                'userType'  => 'administrator',
+                'created_at' => new DateTime,
+                'updated_at' => new DateTime,
+            ],
+            [
+                'username'  => 'secretary',
+                'password'  => bcrypt('secretary'),
+                'employeeName' => 'The Secretary',
+                'position'  => 'Secretary',
+                'idNumber'  => '14102707',
+                'licenseNumber' => '14102707',
+                'userType'  => 'secretary',
+                'created_at' => new DateTime,
+                'updated_at' => new DateTime,
+            ],
+            [
+                'username'  => 'analyst',
+                'password'  => bcrypt('analyst'),
+                'employeeName' => 'The Analyst',
+                'position'  => 'Analyst',
+                'idNumber'  => '14102708',
+                'licenseNumber' => '14102708',
+                'userType'  => 'analyst',
+                'created_at' => new DateTime,
+                'updated_at' => new DateTime,
+            ]
         ]);
     }
 }
