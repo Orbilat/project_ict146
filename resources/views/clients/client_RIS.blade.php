f9dddf480b80d0b277b88d8e74ee9b34f68ad333
unknown <klevincinco@gmail.com>
2019-04-05 12:56:51 +0800

    Lacking Contact

--- resources/views/clients/client_RIS.blade.php
+++ resources/views/clients/client_RIS.blade.php
@@ -4,83 +4,78 @@
 
 
 <div class="container "  style="padding:130px;margin-bottom:80px; ">
-    <div class="row bg-dark w3-padding-16 border border-light">
-        <div class="col-sm-12 text-info">
-            <div class="titleText">RIS NUMBER: 
-                <h3 class="d-inline-block text-danger">
+    <div class="row w3-padding-16" style="background-color:#A1CDA8">
+        <div class="col-sm-12">
+            <div class="text-dark TS">RIS NUMBER: 
+                <div class="d-inline-block text-danger">
                     {{ $ris->risNumber}}
-                </h3>
-                <h4 class="d-inline-block float-right">
-                    Payment:
+                </div>
+                <div class="float-right TS">
+                    Payment:   
                     @if ( $ris->paid == "yes" || $ris->paid == "Yes")
-                        Done
+                    <div class="d-inline-block text-success float-right">Done</div>   
                     @else 
-                        Pending
+                    <div class="d-inline-block text-danger float-right">Pending</div>    
                     @endif
-                </h4>
+                </div> 
             </div>
         </div>
     </div>
-    <div class="row border border-dark">
-        <div class="col-sm-3 bg-dark ">
-            <h4 class="text-center titleText" style="margin-top:15px;">Date</h3>
+    <div class="row" style="background-color:#e9ecef;">
+        <div class="col-sm-3" >
+            <h4 class="text-center text-dark TS" style="margin-top:15px;">Date</h3>
         </div>
-        <div class="col-sm-3 bg-dark">
-            <h4 class="text-center titleText" style="margin-top:15px;">Lab-Code</h3>
+        <div class="col-sm-3">
+            <h4 class="text-center text-dark TS" style="margin-top:15px;">Lab-Code</h3>
         </div>
-        <div class="col-sm-3 bg-dark">
-            <h4 class="text-center titleText" style="margin-top:15px;">Analysis</h3>
+        <div class="col-sm-3">
+            <h4 class="text-center text-dark TS" style="margin-top:15px;">Analysis</h3>
         </div>
-        <div class="col-sm-3 bg-dark">
-            <h4 class="text-center titleText" style="margin-top:15px;">Status</h4>
+        <div class="col-sm-3">
+            <h4 class="text-center text-dark TS" style="margin-top:15px;">Status</h4>
         </div>
-  
-        @foreach($ris->samples as $sample)
-          <div class="col-md-3 w3-border text-center">{{ $sample->managedDate }}</div>
-          <div class="col-md-3 w3-border text-center">{{ $sample->laboratoryCode }}</div>
-          <div class="col-md-3 w3-border text-center">{{ $sample->analysis }}</div>
-          <div class="col-md-3 w3-border text-center">{{ $sample->status }}</div>
-        @endforeach
     </div>
-    <div class="container">
+        @foreach($ris->samples as $sample)
+          <div class="col-md-3 w3-border text-center TS">{{ $sample->managedDate }}</div>
+          <div class="col-md-3 w3-border text-center TS">{{ $sample->laboratoryCode }}</div>
+          <div class="col-md-3 w3-border text-center TS">{{ $sample->analysis }}</div>
+          <div class="col-md-3 w3-border text-center TS">{{ $sample->status }}</div>
+        @endforeach
          @if ( $ris->readyForPickUp == "yes" || $ris->paid == "Yes")
-            <div class="p-5 alert alert-success w3-padding-64 FontError">
-                <i class="glyphicon glyphicon-thumbs-up fa-2x"></i> 
+            <div class="p-5 alert alert-success text-center FontError">
+                <i class="glyphicon glyphicon-ok fa-1x"></i> 
                 <strong>Ready for Pick Up</strong>
             </div>
         @else
-            <div class="p-5 alert alert-danger w3-padding-64 FontError">
-                <i class="fa fa-exclamation-triangle fa-2x"></i> 
+            <div class="p-5 alert alert-danger text-center FontError">
+                <i class="fa fa-exclamation-triangle fa-1x"></i> 
                 <strong>Please settle your account</strong>
             </div>
         @endif
-    </div>
 </div>
-
-           
-        
-
-
-                                                              
-      <footer class="w3-center w3-black w3-padding-32">
-          <div class="w3-section">
-              <div class="col-md-4 text-center"> <h2 class="titleText">Address</h2>
-                  <p class="text-center">Room 320, 3rd Floor Bunzel Building,<br> University of San Carlos Talamban Campus, <br>
+                                                         
+<footer class="w3-center w3-black w3-padding-32">
+          <div class="row">
+            <div class="col-md-1"></div>
+              <div class="col-md-3 text-center"> <h2 class="titleText" style="font-size: 15px;">Address</h2>
+                  <p class="text-center" style="font-size: 10px;">Room 320, 3rd Floor Bunzel Building, University of San Carlos Talamban Campus, 
                   Nasipit Talamban Cebu City Philippines 6000</p>
               </div>
-              <div class="col-md-4 text-center"> <h2 class="titleText">Contacts</h2>
-                  <p class="text-center">Email:waterlab@usc.edu.ph<br>
+              <div class="col-md-4 text-center"> <h2 class="titleText" style="font-size: 15px;">Contacts</h2>
+                  <p class="text-center" style="font-size: 10px;">Email:waterlab@usc.edu.ph<br>
                   Phone: (63 32)345 3811<br>
                   Fax: (63 32)230 0100 loc 110</p>
               </div>
-              <div class="col-md-4 text-center"> <h2 class="titleText">Feedbacks</h2>
-                  <p class="text-center">Please send us your ideas, bug reports, suggestions! <br>
+              <div class="col-md-3 text-center"> <h2 class="titleText" style="font-size: 15px;">Feedbacks</h2>
+                  <p class="text-center" style="font-size: 10px;">Please send us your ideas, bug reports, suggestions! <br>
                   Any feedback would be appreciated.</p>
                   <br>
               </div>
-              <div class=" text-center titleText" style="font-size: 25px;">Connect with Us:
-                  <a href="https://www.facebook.com/pages/USC-Water-Laboratory/618035434997379" style="color: #fff; font-size: 25px;"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
-              </div>
+          </div>
+              <div class="col-md-12 col-xs-12 col-centered w3-black">
+                <div class=" text-center titleText" style="font-size: 15px; align-content: center;" >Connect with us:
+                    <a href="https://www.facebook.com/pages/USC-Water-Laboratory/618035434997379" style="color: #fff; font-size:20px;"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
+                </div>
           </div>
       </footer>
 
--- resources/views/clients/client_S&R.blade.php
+++ resources/views/clients/client_S&R.blade.php
@@ -4,14 +4,14 @@
 <br><br>
 <br><br>
 
-<div class="container" style="margin-bottom:30%;">
+<div class="container" style="margin-bottom:25%;">
     <div class="card-body"> 
         <table class="table">
-            <thead>
+            <thead style="background-color:#A1CDA8;">
                 <tr class="text-center">
-                  <th class="text-light bg-secondary TS">Analysis</th>
-                  <th class="text-light bg-secondary TS">Method</th>
-                  <th class="text-light bg-secondary TS">Price</th>
+                  <th class="text-dark TS">Analysis</th>
+                  <th class="text-dark TS">Method</th>
+                  <th class="text-dark TS">Price</th>
                 </tr>
             </thead>
             <tbody>
@@ -27,25 +27,29 @@
     </div>
 </div>
 
-      <footer class="w3-center w3-black w3-padding-32">
-          <div class="w3-section">
-              <div class="col-md-4 text-center"> <h2 class="titleText">Address</h2>
-                  <p class="text-center">Room 320, 3rd Floor Bunzel Building,<br> University of San Carlos Talamban Campus, <br>
+<footer class="w3-center w3-black w3-padding-32">
+          <div class="row">
+            <div class="col-md-1"></div>
+              <div class="col-md-3 text-center"> <h2 class="titleText" style="font-size: 15px;">Address</h2>
+                  <p class="text-center" style="font-size: 10px;">Room 320, 3rd Floor Bunzel Building, University of San Carlos Talamban Campus, 
                   Nasipit Talamban Cebu City Philippines 6000</p>
               </div>
-              <div class="col-md-4 text-center"> <h2 class="titleText">Contacts</h2>
-                  <p class="text-center">Email:waterlab@usc.edu.ph<br>
+              <div class="col-md-4 text-center"> <h2 class="titleText" style="font-size: 15px;">Contacts</h2>
+                  <p class="text-center" style="font-size: 10px;">Email:waterlab@usc.edu.ph<br>
                   Phone: (63 32)345 3811<br>
                   Fax: (63 32)230 0100 loc 110</p>
               </div>
-              <div class="col-md-4 text-center"> <h2 class="titleText">Feedbacks</h2>
-                  <p class="text-center">Please send us your ideas, bug reports, suggestions! <br>
+              <div class="col-md-3 text-center"> <h2 class="titleText" style="font-size: 15px;">Feedbacks</h2>
+                  <p class="text-center" style="font-size: 10px;">Please send us your ideas, bug reports, suggestions! <br>
                   Any feedback would be appreciated.</p>
                   <br>
               </div>
-              <div class=" text-center titleText" style="font-size: 25px;">Connect with us:
-                  <a href="https://www.facebook.com/pages/USC-Water-Laboratory/618035434997379" style="color: #fff; font-size:30px;"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
-              </div>
           </div>
+            <div class="col-md-12 col-xs-12 col-centered w3-black">
+                <div class=" text-center titleText" style="font-size: 15px; align-content: center;" >Connect with us:
+                    <a href="https://www.facebook.com/pages/USC-Water-Laboratory/618035434997379" style="color: #fff; font-size:20px;"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
+                </div>
+            </div>
       </footer>
+
 @endsection
