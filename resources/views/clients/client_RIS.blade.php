dcc297c07122c822b9c8dfdb0a1a4c4eb31a1288
unknown <klevincinco@gmail.com>
2019-04-18 15:15:50 +0800

    change client view

--- resources/views/clients/client_RIS.blade.php
+++ resources/views/clients/client_RIS.blade.php
@@ -49,7 +49,7 @@
                     <div class="col-md-3 w3-border text-center TS">{{ $parameter->pivot->status }}</div>
                 </div>
         @endforeach
-
+            <br>
         @foreach($clients as $client)
             @if ( $client->readyForPickUp == "yes" || $client->paid == "Yes")
                 <div class="p-5 alert alert-success text-center FontError">
@@ -64,68 +64,20 @@
             @endif
         @endforeach
 </div>
-             
-
-
-
-
-<div class="container "  style="padding-top: 120px;margin-bottom:30px; ">
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Date</th>
-                <th>Lab-Code</th>
-                <th>Analysis</th>
-                <th>Status</th>
-            </tr>
-        </thead>  
-        <tbody>
-        @foreach($client->samples as $sample)
-           
-            <tr>
-
-                    <td>{{ $sample->managedDate  }}</td>
-                    <td>{{ $sample->laboratoryCode  }}</td><td>
-                    @foreach($sample->parameters as $parameter)
-                    {{ $parameter->analysis }}<br>
-                    @endforeach</td>
-                    <td>{{ $parameter->pivot->status }}</td>
-                    
-                    </tr>
-
-           
-        @endforeach
-        </tbody>
-        </table>
-        @foreach($clients as $client)
-            @if ( $client->readyForPickUp == "yes" || $client->paid == "Yes")
-                <div class="p-5 alert alert-success text-center FontError">
-                    <i class="glyphicon glyphicon-ok fa-1x"></i> 
-                    <strong>Ready for Pick Up</strong>
-                </div>
-            @else
-                <div class="p-5 alert alert-danger text-center FontError">
-                    <i class="fa fa-exclamation-triangle fa-1x"></i> 
-                    <strong>Please settle your account</strong>
-                </div>
-            @endif
-        @endforeach
-</div>
-
 
 <footer class="w3-center w3-black w3-padding-32">
           <div class="row">
             <div class="col-md-1"></div>
-              <div class="col-md-3 text-center"> <h2 class="titleText" style="font-size: 15px;">Address</h2>
+              <div class="col-sm-3 text-center"> <h2 class="titleText" style="font-size: 15px;">Address</h2>
                   <p class="text-center" style="font-size: 10px;">Room 320, 3rd Floor Bunzel Building, University of San Carlos Talamban Campus, 
                   Nasipit Talamban Cebu City Philippines 6000</p>
               </div>
-              <div class="col-md-4 text-center"> <h2 class="titleText" style="font-size: 15px;">Contacts</h2>
+              <div class="col-sm-4 text-center"> <h2 class="titleText" style="font-size: 15px;">Contacts</h2>
                   <p class="text-center" style="font-size: 10px;">Email:waterlab@usc.edu.ph<br>
                   Phone: (63 32)345 3811<br>
                   Fax: (63 32)230 0100 loc 110</p>
               </div>
-              <div class="col-md-3 text-center"> <h2 class="titleText" style="font-size: 15px;">Feedbacks</h2>
+              <div class="col-sm-3 text-center"> <h2 class="titleText" style="font-size: 15px;">Feedbacks</h2>
                   <p class="text-center" style="font-size: 10px;">Please send us your ideas, bug reports, suggestions! <br>
                   Any feedback would be appreciated.</p>
                   <br>
--- resources/views/clients/client_home.blade.php
+++ resources/views/clients/client_home.blade.php
@@ -1,16 +1,17 @@
 @extends('layouts.clientapp')
 
+
+<div class="bg-opacity">
 @section('content')
-<br>
-      <div class="w3-container" style="margin:5%;">
-          <div class="container"> 
+<div class="container">
               <div class="row">
-                  <div class="col-md-6">
+                  <div class="col-md-6 ">
                       <h2 class="titleText">ABOUT US</h2>
                       <br>
-                      <p>The University of San Carlos Water Laboratory offers analytical services for water, soil, ore, and other special samples. The Water Laboratory is accredited as a 
-                      testing laboratory by the Department of Environment and Natural Resources (DENR) and the Department of Health (DOH).Construction and Materials Testing Laboratory</p>
-                      <p>The USC Water Laboratory, which accepts third-party analysis and samples from the public for a fee, is attached to the Department of Chemistry.</p>
+                      <p class="text-light containerRow">The University of San Carlos Water Laboratory offers analytical services for water, soil, ore, and other special samples. The Water Laboratory is accredited as a 
+                      testing laboratory by the Department of Environment and Natural Resources (DENR) and the Department of Health (DOH).Construction and Materials Testing Laboratory
+                      <br><br>
+                      The USC Water Laboratory, which accepts third-party analysis and samples from the public for a fee, is attached to the Department of Chemistry.</p>
                   </div>
                   <div class="col-md-6">
                       <h2 class="w3-center titleText">News</h2>
@@ -22,33 +23,89 @@
                   </div>
               </div>
           </div>
-      </div>
+    </div>
                                                                    
-                                                                   
-      <footer class="w3-center w3-black w3-padding-32">
-          <div class="row">
-            <div class="col-md-1"></div>
-              <div class="col-md-3 text-center"> <h2 class="titleText" style="font-size: 15px;">Address</h2>
-                  <p class="text-center" style="font-size: 10px;">Room 320, 3rd Floor Bunzel Building, University of San Carlos Talamban Campus, 
+            
+      <!-- Footer -->
+<footer class="page-footer font-small indigo w3-black text-white">
+
+<!-- Footer Links -->
+<div class="container text-center text-md-left">
+
+  <!-- Grid row -->
+  <div class="row">
+
+    <!-- Grid column -->
+    <div class="col-md-3 mx-auto">
+
+      <!-- Links -->
+      <h5 class="font-weight-bold text-uppercase mt-3 mb-4 ">About</h5>
+          <p>The Water Laboratory is accredited as a testing laboratory by the Department of Environment and Natural Resources (DENR) and the Department of Health (DOH).</p>
+    </div>
+    <!-- Grid column -->
+
+    <hr class="clearfix w-100 d-md-none">
+
+    <!-- Grid column -->
+    <div class="col-md-3 mx-auto">
+
+      <!-- Links -->
+      <h5 class="font-weight-bold text-uppercase mt-3 mb-4 ">Address</h5>
+
+      <p>Room 320, 3rd Floor Bunzel Building, University of San Carlos Talamban Campus, 
                   Nasipit Talamban Cebu City Philippines 6000</p>
-              </div>
-              <div class="col-md-4 text-center"> <h2 class="titleText" style="font-size: 15px;">Contacts</h2>
-                  <p class="text-center" style="font-size: 10px;">Email:waterlab@usc.edu.ph<br>
-                  Phone: (63 32)345 3811<br>
-                  Fax: (63 32)230 0100 loc 110</p>
-              </div>
-              <div class="col-md-3 text-center"> <h2 class="titleText" style="font-size: 15px;">Feedbacks</h2>
-                  <p class="text-center" style="font-size: 10px;">Please send us your ideas, bug reports, suggestions! <br>
+    </div>
+    <!-- Grid column -->
+
+    <hr class="clearfix w-100 d-md-none">
+
+    <!-- Grid column -->
+    <div class="col-md-3 mx-auto">
+
+      <!-- Links -->
+      <h5 class="font-weight-bold text-uppercase mt-3 mb-4 ">Contact Us </h5>
+
+      <p>Email:waterlab@usc.edu.ph<br>
+         Phone: (63 32)345 3811<br>
+         Fax: (63 32)230 0100 loc 110</p>
+    </div>
+    <!-- Grid column -->
+
+    <hr class="clearfix w-100 d-md-none">
+
+    <!-- Grid column -->
+    <div class="col-md-3 mx-auto">
+
+      <!-- Links -->
+      <h5 class="font-weight-bold text-uppercase mt-3 mb-4 ">Feedbacks</h5>
+
+      <p>Please send us your ideas, bug reports, suggestions! <br>
                   Any feedback would be appreciated.</p>
-                  <br>
-              </div>
-          </div>
-              <div class="col-md-12 col-xs-12 col-centered w3-black">
-                <div class=" text-center titleText" style="font-size: 15px; align-content: center;" >Connect with us:
-                    <a href="https://www.facebook.com/pages/USC-Water-Laboratory/618035434997379" style="color: #fff; font-size:20px;"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
-                </div>
-          </div>
-      </footer>
+      <ul class="list-unstyled">
+        <li>
+          <a href="{{ url('/contact') }}">Message Us<i class="glyphicon glyphicon-envelope fa-fw "></i></a>
+        </li>
+      </ul>
+
+    </div>
+    <!-- Grid column -->
+
+  </div>
+  <!-- Grid row -->
+
+</div>
+<!-- Footer Links -->
+<h5 class="text-center"  style="font-size:30px; border-radius: 35px;">Follow us:
+    <a href="https://www.facebook.com/pages/USC-Water-Laboratory/618035434997379"><i class="fa fa-facebook-official w3-hover-opacity " style=" font-size:30px; border-radius: 35px;"></i></a> 
+</h5>
+<!-- Copyright -->
+<div class="footer-copyright text-center py-3">© 2019 Copyright:
+  <a href="https://mdbootstrap.com/education/bootstrap/">USCwaterlab.com</a>- All rights served
+</div>
+<!-- Copyright -->
+
+</footer>
+<!-- Footer -->
 
 
               <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
