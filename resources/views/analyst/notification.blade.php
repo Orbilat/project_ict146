1fd480378569745f6f59de70a4f1bc6004a0b04f
LiteInterns <liteinterns@gmail.com>
2019-04-23 14:54:08 +0800

    edited admin home

--- resources/views/analyst/notification.blade.php
+++ resources/views/analyst/notification.blade.php
@@ -5,8 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-10">
             <div class="card">
-                <div class="card-header ">Notification
-                </div>
+                <div class="card-header ">Notification</div>
                 <br>
                 <table id="sampledata" class="display sampledata table table-hover" style="width:100%">
                     <thead class="thead-light">
