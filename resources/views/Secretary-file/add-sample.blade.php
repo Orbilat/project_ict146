0ae580099f720524518f37bb6700b8e61645a1f5
LiteInterns <liteinterns@gmail.com>
2019-04-26 23:05:34 -0600

    DTM, Select Client Changed

--- resources/views/Secretary-file/add-sample.blade.php
+++ resources/views/Secretary-file/add-sample.blade.php
@@ -34,7 +34,7 @@
                     <form action="{{ route('postAddSample') }}" method="post">
                         @csrf
                         <div class="form-group row">
-                            <label for="clientId" class="col-md-4 col-form-label text-md-right">{{ __('Client RIS') }}</label>
+                            <label for="clientId" class="col-md-4 col-form-label text-md-right">{{ __('Client') }}</label>
                             
                             <div class="col-md-6">
                                 <select class="form-control" id="clientId" name="clientId" required autofocus>
